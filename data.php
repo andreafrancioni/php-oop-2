dff45f12400e62ae089cb4bb74d195cef7dfbde1
andreafrancioni <andreafrancioni.work@gmail.com>
2024-01-16 16:43:45 +0100

    Categoria definita in data

--- data.php
+++ data.php
@@ -5,23 +5,26 @@ require_once __DIR__ . '/Models/Cibo.php';
 require_once __DIR__ . '/Models/Cucce.php';
 require_once __DIR__ . '/Models/Giochi.php';
 
+$dog = new Category("cane", "fas fa-dog");
+$cat = new Category("gatto", "fas fa-cat");
+
 $products = [
     new Giochi(
         "Palla con squeak",
         5.99,
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJLx_IarI58TC2gd1KatKHsAgETideratTPg&usqp=CAU",
-        new Category("cane", "https://cdn-icons-png.flaticon.com/128/194/194279.png")
+        $cane
     ),
     new Cibo(
         "Croccantini per gatti obesi",
         29.99,
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbobacE-V7UY2ohFnyzji_TEE_5ctNODSK0w&usqp=CAU",
-        new Category("gatto", "https://cdn-icons-png.flaticon.com/128/2324/2324723.png")
+        $gatto
     ),
     new Cucce(
         "Cuccia morbida da interno",
         18.99,
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFaoM4ZqvZ75CAqmLcxhlPcu7D1loFFJkr7g&usqp=CAU",
-        new Category("cane", "https://cdn-icons-png.flaticon.com/128/194/194279.png")
+        $cane
     )
 ];
